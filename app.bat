2f7c3ec028e284c7556b92a7f359d1cef4f7cb37
angelokofficial <angelok2015.mc@gmail.com>
2017-09-24 20:36:17 +0300

    launcher

--- app.bat
+++ app.bat
@@ -1,17 +1,25 @@
 @echo off
 title ADB CLI by Angelok
 set title=                      Android Debug Bridge CLI Application
-set version=                                  v0.1 beta
+set version=                                  v0.2 beta
 set dashes=--------------------------------------------------------------------------------
+set txt1=Главное меню
 echo %title%
 echo %version%
 echo %dashes%
+echo %txt1%
 :m1
+echo.
 echo [1] Подключение устройства
-echo [2] Выход
+echo [2] Передача файлов
+echo.
+echo [3] Режим суперпользователя
+echo [4] Выход
 echo.
 set /p choice="Выбор: "
-if "%choice%"=="1" (cd bin/batfiles && start connect.bat && exit)
-if "%choice%"=="2" (exit)
+if "%choice%"=="1" (cd bin/modules && cls && connect && exit)
+if "%choice%"=="2" (cd bin/modules && cls && pushpull && exit)
+if "%choice%"=="3" (cd bin/modules && cls && rootmode && exit)
+if "%choice%"=="4" (exit)
 goto m1
 pause
